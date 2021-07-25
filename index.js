a17a3a40d8f2847e3492af54e5ad0a0ec094038a
joshraiborde <joshraiborde@gmail.com>
2021-07-25 17:36:40 -0400

    created index js file and added funciton app

---
+++ index.js
@@ -0,0 +1,8 @@
+function App() {
+    return (
+        <div className="container"></div>
+        <div className="grid"></div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
