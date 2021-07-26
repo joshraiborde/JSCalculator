827402c5edb1d9d14a6d58d3657372b01667d6b3
joshraiborde <joshraiborde@gmail.com>
2021-07-26 08:30:32 -0400

    added color to buttons

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ function App() {
         <div className="container">
         <div className="grid">
         <div className="dis"></div>
-        <div className="padButton AC">AC</div>
-        <div className="padButton C">C</div>
+        <div className="padButton AC tomato">AC</div>
+        <div className="padButton C tomato">C</div>
         <div className="padButton divide">/</div>
         <div className="padButton multiply">x</div>
-        <div className="padButton seven">7</div>
-        <div className="padButton eight">8</div>
-        <div className="padButton nine">9</div>
+        <div className="padButton seven dark-gray">7</div>
+        <div className="padButton eight dark-gray">8</div>
+        <div className="padButton nine dark-gray">9</div>
         <div className="padButton minus">-</div>
-        <div className="padButton four">4</div>
-        <div className="padButton five">5</div>
-        <div className="padButton six">6</div>
+        <div className="padButton four dark-gray">4</div>
+        <div className="padButton five dark-gray">5</div>
+        <div className="padButton six dark-gray">6</div>
         <div className="padButton plus">+</div>
-        <div className="padButton one">1</div>
-        <div className="padButton two ">2</div>
-        <div className="padButton three">3</div>
-        <div className="padButton equal">=</div>
+        <div className="padButton one dark-gray">1</div>
+        <div className="padButton two dark-gray">2</div>
+        <div className="padButton three dark-gray">3</div>
+        <div className="padButton equal blue">=</div>
         <div className="padButton zero">0</div>
         <div className="padButton dot">.</div>
         </div>
@@ -27,4 +27,3 @@ function App() {
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
