cbe7a09dde999fd38b21bba8d64fd2571327d63e
joshraiborde <joshraiborde@gmail.com>
2021-07-26 12:28:33 -0400

    added feature to reset display after calculation

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ function App() {
 
     const [expression, setExpression] = React.useState("");
     const [answer, setAnswer] = React.useState(0);
+    
     const display = (symbol) => {
         setExpression(prev => prev + symbol)
+        if(expression[expression.length-1] == "="){
+            if(/[0-9]/.test(symbol)){
+                setExpression(symbol)
+            }else{
+                setExpression(answer + symbol)
+            }
+        }
     }
 
     const calculate = () =>{
         setAnswer(eval(expression))
+        setExpression((prev) => prev + "=")
     }
 
     const allClear = () => {
